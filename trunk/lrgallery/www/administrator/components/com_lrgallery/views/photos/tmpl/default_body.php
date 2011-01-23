0f2fbf7a41d1a7a6c87bbb89e8ab2d9dbb499312
smArtCSF <smArtCSF@1fd71581-f900-ad69-53c7-e43be6c9a8d8>
2011-01-23 14:09:44 +0000

    Установил компонент с JTable

---
+++ trunk/lrgallery/www/administrator/components/com_lrgallery/views/photos/tmpl/default_body.php
@@ -0,0 +1,23 @@
+<?php
+    defined('_JEXEC') or die('Restricted Access');
+?>
+
+<?php foreach ($this->items as $i => $item): ?>
+    <tr class="row<?php echo $i % 2; ?>">
+        <td align="center">
+            <?php echo JHtml::_('grid.id', $i, $item->id); ?>
+        </td>
+        <td>
+            <?php echo $item->user_name; ?>
+        </td>
+        <td>
+            <?php echo $item->name; ?>
+        </td>
+        <td>
+            <?php echo $item->file_name; ?>
+        </td>
+        <td align="center">
+            <?php echo $item->id; ?>
+        </td>                
+    </tr>
+<?php endforeach; ?>
---
+++ trunk/lrgallery/www/components/com_lrgallery/models/lrgallery.php
@@ -0,0 +1,20 @@
+<?php
+
+    defined('_JEXEC') or die('Restricted access');
+
+    jimport('joomla.application.component.modelitem');
+
+    class LrgalleryModelLrgallery extends JModelItem
+    {
+        protected $msg;
+
+        public function getMsg() 
+        {
+            if (!isset($this->msg)) 
+            {
+                $this->msg = 'Hello World!';
+            }
+            return $this->msg;
+        }
+    }
+?>
