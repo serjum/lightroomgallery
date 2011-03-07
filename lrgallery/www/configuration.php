1507caae70e5a295bf9406a6b4be2ffff7138568
smArtCSF <smArtCSF@1fd71581-f900-ad69-53c7-e43be6c9a8d8>
2011-03-07 19:18:02 +0000

    Установил компонент Установил шаблон yougrids

--- lrgallery/www/configuration.php
+++ lrgallery/www/configuration.php
@@ -18,7 +18,7 @@ class JConfig {
 	public $secret = 'O3hvsew1iQnLjYyb';
 	public $gzip = '0';
 	public $error_reporting = '-1';
-	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&amp;keyref=Help{major}{minor}:{keyref}';
+	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&keyref=Help{major}{minor}:{keyref}';
 	public $ftp_host = '127.0.0.1';
 	public $ftp_port = '21';
 	public $ftp_user = '';
@@ -32,8 +32,8 @@ class JConfig {
 	public $fromname = 'Lightroom Gallery';
 	public $sendmail = '/usr/sbin/sendmail';
 	public $smtpauth = '0';
-	public $smtpuser = '';
-	public $smtppass = '';
+	public $smtpuser = 'admin';
+	public $smtppass = 'ad1min2';
 	public $smtphost = 'localhost';
 	public $smtpsecure = 'none';
 	public $smtpport = '25';
@@ -47,10 +47,16 @@ class JConfig {
 	public $sef = '1';
 	public $sef_rewrite = '0';
 	public $sef_suffix = '0';
-	public $unicodeslugs = '0';
+	public $unicodeslugs = '1';
 	public $feed_limit = '10';
 	public $log_path = 'Z:\\home\\lrgallery\\www\\logs';
 	public $tmp_path = 'Z:\\home\\lrgallery\\www\\tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
+	public $MetaRights = '';
+	public $sitename_pagetitles = '1';
+	public $force_ssl = '0';
+	public $feed_email = 'author';
+	public $cookie_domain = '';
+	public $cookie_path = '';
 }
