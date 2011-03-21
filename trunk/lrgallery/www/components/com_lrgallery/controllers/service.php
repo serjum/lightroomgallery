fdd6e01783220e16b94114f34b9c58e9941af87c
smArtCSF <smArtCSF@1fd71581-f900-ad69-53c7-e43be6c9a8d8>
2011-03-21 18:53:31 +0000

    Начал метод login()

--- trunk/lrgallery/www/components/com_lrgallery/controllers/service.php
+++ trunk/lrgallery/www/components/com_lrgallery/controllers/service.php
@@ -3,12 +3,44 @@
     defined('_JEXEC') or die;
 
     jimport('joomla.application.component.controllerform');
+    jimport('joomla.user.user');
+    jimport('joomla.access.access');
 
     class LrgalleryControllerService extends JControllerForm
     {
-        public function login()
+        /*
+         * ID группы администраторов
+         */
+        const adminGroupID = 7;
+        
+        /*
+         * Вход в систему
+         * Используются имя пользователя и пароль пользователя из группы администраторов
+         */
+        public function login($username, $password)
         {
+            // Пытаемся найти указанного пользователя
+            $user =& JFactory::getUser($username);            
+            if ($user == null)
+                return false;
             
+            // Если пользователь есть, проверяем, входит ли он в административную группу
+            $acl =& JFactory::getACL();
+            $userGroups = JAccess::getGroupsByUser($user->getParam('id'));
+            if (!in_array(self::adminGroupID, $userGroups))
+                return false;
+            
+            // Если пользователь входит в группу администраторов, пробуем аутентифицировать его            
+            $credentials = array();
+            $credentials['username'] = $username;
+            $credentials['password'] = $password;
+            $app = JFactory::getApplication();
+            $error = $app->login($credentials);
+            if (JError::isError($error))
+                return false;
+            
+            // Если всё в порядке, сгенерируем новый токен, внесём в базу и вернём его
+            $token = md5(date());
         }
         
         public function createUser($username, $password, $folderName)
