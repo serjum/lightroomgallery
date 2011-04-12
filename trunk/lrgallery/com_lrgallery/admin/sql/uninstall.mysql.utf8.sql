dbdc4f6a3ab468cd9e8fec69d02a15e33068191d
smArtCSF <smArtCSF@1fd71581-f900-ad69-53c7-e43be6c9a8d8>
2011-04-12 18:06:20 +0000

    Собрал компонент

--- trunk/lrgallery/com_lrgallery/admin/sql/uninstall.mysql.utf8.sql
+++ trunk/lrgallery/com_lrgallery/admin/sql/uninstall.mysql.utf8.sql
@@ -2,5 +2,5 @@ drop table if exists #__lrgallery_userfolders;
 drop table if exists #__lrgallery_metadata;
 drop table if exists #__lrgallery_meta;
 drop table if exists #__lrgallery_photos;
-
+drop table if exists #__lrgallery_tokens;
 
